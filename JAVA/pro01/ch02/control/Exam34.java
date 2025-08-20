45c0a7c9e60c045b9f84d99ac7a322a5ad7aed33
DESKTOP-LBEO3IF\JMH <lucia88@hanmail.net>
2025-08-20 20:44:18 +0900

    VER12

--- JAVA/pro01/ch02/control/Exam34.java
+++ JAVA/pro01/ch02/control/Exam34.java
@@ -10,26 +10,6 @@ import java.io.IOException;
 
 public class Exam34 {
 	public static void main(String[] ar)throws IOException{
-		System.out.print("문자를 입력해주세요:");
-		int a=System.in.read();
-		System.out.println("a:" + a);
-		
-		System.out.print("문자를 입력해주세요:");
-		char x=(char)System.in.read();
-		System.out.println("x:" + x);
-		System.in.read();
-		System.in.read();
-		System.out.println();
-				
-		System.out.print("문자를 입력해주세요:");
-		char y=(char)System.in.read();
-		System.in.read();	
-		System.in.read();	
-		System.out.println("y:" + y);
-		System.out.println();
-				
-		System.out.print("문자를 입력해주세요:");
-		char z=(char)System.in.read();
-		System.out.println("z:" + z);		
+	
 	}
 }
